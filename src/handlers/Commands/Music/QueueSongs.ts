b7b1fa7b0cd8bf3479fd25474456030fd4b680ce
snippik <xdox.xdox@yandex.ru>
2025-01-12 18:41:53 +0300

    Переработка команды skip - Теперь имеет 3 действия - Действие back - переключение позиции назад - Действие to - глобальное переключение позиции - Действие next - переключение позиции вперед

--- src/handlers/Commands/Music/QueueSongs.ts
+++ src/handlers/Commands/Music/QueueSongs.ts
@@ -7,10 +7,10 @@ import {db} from "@lib/db";
 /**
  * @author SNIPPIK
  * @description Пропуск треков до указанного трека!
- * @class SkipTracksCommand
+ * @class SkipUtilityCommand
  * @public
  */
-class SkipTracksCommand extends Constructor.Assign<Handler.Command> {
+class SkipUtilityCommand extends Constructor.Assign<Handler.Command> {
     public constructor() {
         super({
             builder: new SlashBuilder()
@@ -20,24 +20,87 @@ class SkipTracksCommand extends Constructor.Assign<Handler.Command> {
                 })
                 .setDescription({
                     "en-US": "Skip tracks to the specified track! The specified track will be current!",
-                    "ru": "Пропуск треков до указанного трека! Указанный трек будет текущим!"
+                    "ru": "Универсальная команда для управления позицией трека!"
                 })
                 .addSubCommands([
                     {
-                        type: ApplicationCommandOptionType["Number"],
-                        required: true,
                         names: {
-                            "en-US": "value",
-                            "ru": "число"
+                            "en-US": "back",
+                            "ru": "назад"
                         },
                         descriptions: {
-                            "en-US": "Number track in queue!",
-                            "ru": "Номер трека!"
-                        }
+                            "en-US": "Move current track to past!",
+                            "ru": "Переход от текущего трека к прошлому!"
+                        },
+                        type: ApplicationCommandOptionType.Subcommand,
+                        options: [
+                            {
+                                names: {
+                                    "en-US": "value",
+                                    "ru": "число"
+                                },
+                                descriptions: {
+                                    "en-US": "You need to specify the track number!",
+                                    "ru": "Нужно указать номер трека!"
+                                },
+                                required: true,
+                                type: ApplicationCommandOptionType["Number"]
+                            }
+                        ]
+                    },
+                    {
+                        names: {
+                            "en-US": "to",
+                            "ru": "на"
+                        },
+                        descriptions: {
+                            "en-US": "Go to the specified track!",
+                            "ru": "Переход к указанному треку!"
+                        },
+                        type: ApplicationCommandOptionType.Subcommand,
+                        options: [
+                            {
+                                names: {
+                                    "en-US": "value",
+                                    "ru": "число"
+                                },
+                                descriptions: {
+                                    "en-US": "You need to specify the track number!",
+                                    "ru": "Нужно указать номер трека!"
+                                },
+                                required: true,
+                                type: ApplicationCommandOptionType["Number"]
+                            }
+                        ]
+                    },
+                    {
+                        names: {
+                            "en-US": "next",
+                            "ru": "вперед"
+                        },
+                        descriptions: {
+                            "en-US": "Skip tracks to the specified track!",
+                            "ru": "Пропуск треков до указанного трека!"
+                        },
+                        type: ApplicationCommandOptionType.Subcommand,
+                        options: [
+                            {
+                                names: {
+                                    "en-US": "value",
+                                    "ru": "число"
+                                },
+                                descriptions: {
+                                    "en-US": "You need to specify the track number!",
+                                    "ru": "Нужно указать номер трека!"
+                                },
+                                required: true,
+                                type: ApplicationCommandOptionType["Number"]
+                            }
+                        ]
                     }
                 ]),
             rules: ["voice", "another_voice", "queue", "player-not-playing"],
-            execute: ({message, args}) => {
+            execute: ({message, args, type}) => {
                 const number = args.length > 0 ? parseInt(args.pop()) : 1;
                 const {player, tracks} = db.audio.queue.get(message.guild.id);
 
@@ -47,88 +110,64 @@ class SkipTracksCommand extends Constructor.Assign<Handler.Command> {
                     return;
                 }
 
-                // Если пользователь укажет больше чем есть в очереди или меньше
-                else if (number > tracks.size || number < 1) {
-                    message.fastBuilder = { description: locale._(message.locale, "command.seek.duration.big"), color: Colors.DarkRed };
-                    return;
-                }
+                switch (type) {
+                    // Переключение текущий позиции назад
+                    case "back": {
+                        // Если пользователь укажет больше чем есть в очереди или меньше
+                        if (number > tracks.size || number < 1) {
+                            message.fastBuilder = { description: locale._(message.locale, "command.seek.duration.big"), color: Colors.DarkRed };
+                            return;
+                        }
 
-                const {title, url, color} = tracks.get(number > 1 ? tracks.position + number - 1 : tracks.position - 1);
+                        const {title, url, color} = tracks.get(number > 1 ? number : number - 1);
 
-                // Если аргумент больше 1, то ищем трек
-                if (number > 1) {
-                    // Меняем позицию трека в очереди
-                    player.stop(tracks.position + number - 1);
-                    message.fastBuilder = { description: locale._(message.locale, "command.skip.arg.track", [number, `[${title}](${url})`]), color };
-
-                    return;
-                }
-
-                // Пропускаем текущий трек
-                player.stop(tracks.position + 1);
-                message.fastBuilder = { description: locale._(message.locale, "command.skip.one.track", [`[${title}](${url})`]), color };
-                return;
-            }
-        });
-    };
-}
-
-/**
- * @author SNIPPIK
- * @description Возврат к конкретному треку
- * @class BackTrackCommand
- * @public
- */
-class BackTrackCommand extends Constructor.Assign<Handler.Command> {
-    public constructor() {
-        super({
-            builder: new SlashBuilder()
-                .setName({
-                    "en-US": "back",
-                    "ru": "назад"
-                })
-                .setDescription({
-                    "en-US": "Move current track to past",
-                    "ru": "Переход от текущего трека к прошлому"
-                })
-                .addSubCommands([
-                    {
-                        names: {
-                            "en-US": "value",
-                            "ru": "число"
-                        },
-                        descriptions: {
-                            "en-US": "You need to specify the track number!",
-                            "ru": "Нужно указать номер трека!"
-                        },
-                        required: true,
-                        type: ApplicationCommandOptionType["Number"]
+                        // Меняем позицию трека в очереди
+                        player.stop(number - 1);
+                        message.fastBuilder = { description: locale._(message.locale, "command.position", [number, `[${title}](${url})`]), color };
+                        return;
                     }
-                ]),
-            rules: ["voice", "another_voice", "queue", "player-not-playing"],
-            execute: ({message, args}) => {
-                const queue = db.audio.queue.get(message.guild.id);
-                const {tracks} = queue;
-                const number = args.length > 0 ? parseInt(args.pop()) : 1;
 
-                // Если аргумент не является числом
-                if (isNaN(number)) {
-                    message.fastBuilder = { description: locale._(message.locale, "command.seek.duration.nan"), color: Colors.DarkRed };
-                    return;
+                    // Переключение текущий позиции в любую сторону
+                    case "to": {
+                        // Если пользователь укажет больше чем есть в очереди или меньше
+                        if (number > tracks.total || number < 1) {
+                            message.fastBuilder = { description: locale._(message.locale, "command.seek.duration.big"), color: Colors.DarkRed };
+                            return;
+                        }
+
+                        const {title, url, color} = tracks.get(number - 1);
+
+                        // Пропускаем текущий трек
+                        player.stop(number - 1);
+                        message.fastBuilder = { description: locale._(message.locale, "command.go.track", [`[${title}](${url})`]), color };
+                        return;
+                    }
+
+                    // Переключение текущий позиции вперед
+                    case "next": {
+                        // Если пользователь укажет больше чем есть в очереди или меньше
+                        if (number > tracks.size || number < 1) {
+                            message.fastBuilder = { description: locale._(message.locale, "command.seek.duration.big"), color: Colors.DarkRed };
+                            return;
+                        }
+
+                        const {title, url, color} = tracks.get(number - 1);
+
+                        // Если аргумент больше 1, то ищем трек
+                        if (number > 1) {
+                            // Меняем позицию трека в очереди
+                            player.stop(tracks.position + number - 1);
+                            message.fastBuilder = { description: locale._(message.locale, "command.skip.arg.track", [number, `[${title}](${url})`]), color };
+
+                            return;
+                        }
+
+                        // Пропускаем текущий трек
+                        player.stop(tracks.position + 1);
+                        message.fastBuilder = { description: locale._(message.locale, "command.skip.one.track", [`[${title}](${url})`]), color };
+                        return;
+                    }
                 }
-
-                // Если пользователь укажет больше чем есть в очереди или меньше
-                else if (number > tracks.size || number < 1) {
-                    message.fastBuilder = { description: locale._(message.locale, "command.seek.duration.big"), color: Colors.DarkRed };
-                    return;
-                }
-
-                const {title, url, color} = tracks.get(number > 1 ? number : number - 1);
-
-                // Меняем позицию трека в очереди
-                queue.player.stop(number);
-                message.fastBuilder = { description: locale._(message.locale, "command.position", [number, `[${title}](${url})`]), color };
-                return;
             }
         });
     };
@@ -203,4 +242,4 @@ class RemoveTrackCommand extends Constructor.Assign<Handler.Command> {
  * @export default
  * @description Делаем классы глобальными
  */
-export default Object.values({SkipTracksCommand, BackTrackCommand, RemoveTrackCommand});
+export default Object.values({SkipUtilityCommand, RemoveTrackCommand});
