4246e5ce4bef6591f59dc42e48a5c5aae3d00851
snippik <xdox.xdox@yandex.ru>
2025-01-17 23:32:03 +0300

    Забыл отвязать некоторые классы из discord/index

--- src/utils/discord/index.ts
+++ src/utils/discord/index.ts
@@ -3,9 +3,6 @@ export {Interact} from "./Utils/Interact";
 export {ShardManager} from "./ShardManager"
 
 export {EmbedBuilder, MessageSendOptions, MessageComponent} from "./Utils/EmbedBuilder";
-export {MessageUtils, IGNORED_ERRORS} from "./Utils/MessageUtils";
-export {InteractionUtils} from "./Utils/InteractionUtils";
-
 import type { Message, BaseInteraction} from "discord.js"
 
 /**
