a3efbac8f54871d2f682a17433cd39d2f8573186
snippik <xdox.xdox@yandex.ru>
2025-05-29 18:45:33 +0300

    Уменьшение строк в VoiceManager.ts из-за voice/adapter.ts

--- src/structures/discord/modules/VoiceManager.ts
+++ src/structures/discord/modules/VoiceManager.ts
@@ -1,5 +1,6 @@
-import {GatewayVoiceServerUpdateDispatchData, GatewayVoiceStateUpdateDispatchData} from "discord-api-types/v10";
-import {DiscordClient} from "../Client";
+import type { GatewayVoiceServerUpdateDispatchData, GatewayVoiceStateUpdateDispatchData } from "discord-api-types/v10";
+import type { DiscordGatewayAdapterCreator, DiscordGatewayAdapterLibraryMethods } from "#service/voice/adapter";
+import type { DiscordClient } from "../Client";
 
 /**
  * @author SNIPPIK
@@ -69,50 +70,3 @@ export class VoiceManager<Client extends DiscordClient> {
         this.adapters.get(payload.guild_id)?.onVoiceStateUpdate(payload);
     };
 }
-
-/**
- * @description Шлюз Discord Адаптер, шлюза Discord.
- * @interface DiscordGatewayAdapterLibraryMethods
- */
-export interface DiscordGatewayAdapterLibraryMethods {
-    /**
-     * @description Call this when the adapter can no longer be used (e.g. due to a disconnect from the main gateway)
-     */
-    destroy(): void;
-    /**
-     * @description Call this when you receive a VOICE_SERVER_UPDATE payload that is relevant to the adapter.
-     * @param data - The inner data of the VOICE_SERVER_UPDATE payload
-     */
-    onVoiceServerUpdate(data: GatewayVoiceServerUpdateDispatchData): void;
-    /**
-     * @description Call this when you receive a VOICE_STATE_UPDATE payload that is relevant to the adapter.
-     * @param data - The inner data of the VOICE_STATE_UPDATE payload
-     */
-    onVoiceStateUpdate(data: GatewayVoiceStateUpdateDispatchData): void;
-}
-
-/**
- * @description Методы, предоставляемые разработчиком адаптера Discord Gateway для DiscordGatewayAdapter.
- * @interface DiscordGatewayAdapterImplementerMethods
- */
-export interface DiscordGatewayAdapterImplementerMethods {
-    /**
-     * @description Это будет вызвано voice, когда адаптер можно будет безопасно уничтожить, поскольку он больше не будет использоваться.
-     */
-    destroy(): void;
-    /**
-     * @description Реализуйте этот метод таким образом, чтобы данная полезная нагрузка отправлялась на основное соединение Discord gateway.
-     * @param payload - Полезная нагрузка для отправки на основное соединение Discord gateway
-     * @returns `false`, если полезная нагрузка определенно не была отправлена - в этом случае голосовое соединение отключается
-     */
-    sendPayload(payload: any): boolean;
-}
-
-/**
- * Функция, используемая для создания адаптеров. Она принимает параметр methods, содержащий функции, которые
- * могут быть вызваны разработчиком при получении новых данных по его шлюзовому соединению. В свою очередь,
- * разработчик вернет некоторые методы, которые может вызывать библиотека - например, для отправки сообщений на
- * шлюз или для подачи сигнала о том, что адаптер может быть удален.
- * @type DiscordGatewayAdapterCreator
- */
-export type DiscordGatewayAdapterCreator = ( methods: DiscordGatewayAdapterLibraryMethods) => DiscordGatewayAdapterImplementerMethods;
