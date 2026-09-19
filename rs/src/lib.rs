c19ddc7f83a9c5f89700f6e05f400b900890b6d8
snippik <xdox.xdox@yandex.ru>
2026-09-19 13:51:15 +0300

    Исправление сборки Android

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,7 +1,9 @@
 mod structures;
 mod utils;
 
+#[cfg(not(target_os = "android"))]
 use mimalloc::MiMalloc;
 
+#[cfg(not(target_os = "android"))]
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
